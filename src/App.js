e6e869b5bd39d260d21eb4a71fbd881a88cc782a
riteshbondre <ritesh.bondre@sodelsolutions.com>
2021-12-13 00:02:28 +0530

    alot to do

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from 'react-bootstrap/Table'
-import { Card,Pagination } from "react-bootstrap";
+import { Card, Pagination } from "react-bootstrap";
+import ReactPagination from "react-paginate";
+
+
 
 function App() {
   const [posts, setPosts] = useState([]);
-  const items = [1,2,3,4,5]
-  const fetch_data = async () => {
+  const items = [1, 2, 3, 4, 5, 6]
+  var a = []
+  let myAll = []
+  // let setData=[]
+  React.useEffect(() => {
     console.log("ybl_oage1");
-    await fetch(
+    fetch(
       `https://hn.algolia.com/api/v1/search_by_date?query=tags=story&page=0`
     )
       .then((res) => res.json())
       .then((data) => {
         setPosts(data);
-        console.log(posts);
       });
-  };
+  }, 1)
+
+  const AddToLocal=()=>{
+    localStorage.setItem("all", JSON.stringify(posts))
+    // localStorage.setItem("a", JSON.stringify(a))
+  }
+  
+  localStorage.setItem("a", JSON.stringify(a))
+
+  const AddPage=()=>{
+    
+    
+    a = [JSON.parse(localStorage.getItem("all"))]
+    
+    a.push(posts)
+    localStorage.setItem("a", JSON.stringify(a))
+    console.log(a)
+  }
+  
+
+
+
+  const handleChange = (data) => {
+    console.log("changed page", data.selected)
+  }
 
   return (
     <div className="App">
@@ -24,38 +53,56 @@ function App() {
       <div className="App">
         {Object.keys(posts).length !== 0 ? (
           <div>
-            {posts?.hits.map((post,index) => {
+            {posts?.hits.map((post, index) => {
               return (
                 <div>
-                <Card key={index} style={{ width: '40rem' ,alignItems:"center", justifyContent:'center'}}>
-                  <Table  striped bordered hover >
-                    <thead>
-                      <tr>
-                      <th>id</th>
-                        <th>Author</th>
-                        <th>Date</th>
-                        <th>Title</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                      <td>{index}</td>
-                        <td onClick={(e) =>{console.log("event")}}>{post.author}</td>
-                        <td>{post.created_at}</td>
-                        <td>{post.title}</td>
-                      </tr>
-                    </tbody>
-                  </Table>
-                  <Pagination>{items}</Pagination>
-                </Card>
-                <Pagination>{items}</Pagination>
+                  <Card key={index} style={{ width: '40rem', alignItems: "center", justifyContent: 'center' }}>
+                    <Table striped bordered hover >
+                      <thead>
+                        <tr>
+                          <th>id</th>
+                          <th>Author</th>
+                          <th>Date</th>
+                          <th>Title</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>{index}</td>
+                          <td onClick={(e) => { console.log("event") }}>{post.author}</td>
+                          <td>{post.created_at}</td>
+                          <td>{post.title}</td>
+                        </tr>
+                      </tbody>
+                    </Table>
+                  </Card>
                 </div>
+
               );
             })}
+            <Pagination onClick={handleChange}>
+              <Pagination.Prev />
+              {items.map((items, index) => {
+                return (
+                  <div key={index}>
+                    <Pagination.Item>{items}</Pagination.Item>
+                  </div>
+                )
+              })
+
+              }
+
+
+              <Pagination.Next />
+            </Pagination>
+            {/* <ReactPagination pageRangeDisplayed={5} pageCount={items} previousLabel={'Next'} previousLabel={'Previous'} /> */}
+
           </div>
+
         ) : null}
       </div>
-      <button onClick={fetch_data}>Fetch</button>
+      <button onClick={AddToLocal}>add to local </button>
+      <button onClick={AddPage}>add</button>
     </div>
   );
 }
